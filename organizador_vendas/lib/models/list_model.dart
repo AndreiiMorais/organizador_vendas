603350a35976bc84d02b14e5b28652af8ee67744
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-06 17:09:46 -0300

    created model

---
+++ organizador_vendas/lib/models/list_model.dart
@@ -0,0 +1,7 @@
+class SalesModel {
+  int? id;
+  String name = '';
+  String address = '';
+  String quant = '';
+  String obs = '';
+}
--- organizador_vendas/lib/repositories/sales_repository.dart
+++ organizador_vendas/lib/repositories/sales_repository.dart
@@ -1,1 +1,8 @@
-
+class SalesReposiroty {
+  String tableName = 'tbl_sales';
+  String colId = 'id';
+  String colName = 'name';
+  String colAddress = 'address';
+  String colQuant = 'quant';
+  String colObs = 'obs';
+}
